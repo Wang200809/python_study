f0f4e356cfc4dd2c55533df62894175757e9dbf8
wcs <wcshan2008@gmail.com>
2026-02-03 23:56:12 +0800

    resolve bug in list assignment 8.5: not include "From:"

--- Assignment_PY4E/list.py
+++ Assignment_PY4E/list.py
@@ -34,7 +34,8 @@
 # From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 # You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message).
 # Then print out a count at the end.
-# Hint: make sure not to include the lines that start with 'From:'. Also look at the last line of the sample output to see how to print the count.
+# Hint: make sure not to include the lines that start with 'From:'.
+# Also look at the last line of the sample output to see how to print the count.
 #
 # You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
 fname = input("Enter file name: ")
@@ -45,13 +46,10 @@ fh = open(fname)
 count = 0
 for line in fh:
     line = line.rstrip()
-    if not line.startswith("From"):
-        continue
-    words=line.split()
-    if len(words)<=2:
-        continue
-    count = count+1
-    print(words[1])
+    if line.startswith("From "):
+        words=line.split()
+        count = count+1
+        print(words[1])
 print("There were", count, "lines in the file with From as the first word")
 
 
